9ed23cb860e4bc06a2e9615934262288d5948b10
Sebastian Rodriguez <r99@melao.de>
2025-06-09 14:16:54 +0200

    Fix to Database

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -4,5 +4,6 @@ set -x
 
 # Chown the mounted data volume
 chown -R service:service "../data/"
+sh db/start_db.sh
 # Launch our service as user 'service'
 exec su -s /bin/sh -c './target/release/backend' service
