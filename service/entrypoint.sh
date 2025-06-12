fd91e90346b9c4f60039e0cb91ca911175efeb75
Sebastian Rodriguez <r99@melao.de>
2025-06-12 14:35:30 +0200

    Hope it works

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -2,9 +2,12 @@
 set -e
 set -x
 
+sh db/start_db.sh
 # Chown the mounted data volume
 chown -R service:service "../data/"
 chown -R service:service /service/data
-sh db/start_db.sh
+
+
+
 # Launch our service as user 'service'
 exec su -s /bin/sh -c './target/release/backend' service
