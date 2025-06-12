68ad8c5b5029b3cbf0807d79eccf4b372fec93dc
Sebastian Rodriguez <r99@melao.de>
2025-06-12 19:05:43 +0200

    cleanup script

--- service/cleanup.py
+++ service/cleanup.py
@@ -1,14 +1,39 @@
 import sqlite3
 import time
 import datetime
+import os 
+from pathlib import Path
+
+DELETE_TIMEINTEVALL = 15
+
+def delete_old_files(directory: str, age_minutes: int = 15):
+    """
+    Delete all files in `directory` older than `age_minutes`.
+    """
+    cutoff = time.time() - age_minutes * 60
+    dir_path = Path(directory)
+
+    for path in dir_path.iterdir():
+        if path.is_file():
+            mtime = path.stat().st_mtime
+            if mtime < cutoff:
+                try:
+                    path.unlink()
+                    print(f"Deleted: {path}")
+                except Exception as e:
+                    print(f"Error deleting {path}: {e}")
 
 while True:
-    connection = sqlite3.connect("/data/parcerotv.db")
+    connection = sqlite3.connect("/service/data/parcerotv.db")
     cursor = connection.cursor()
 
-    cutoff_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=15)
-    cursor.execute("DELETE FROM videos WHERE creation_at < ?", (cutoff_time.strftime("%Y-%m-%d %H:%M:%S"), ))
+    cutoff_time = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=DELETE_TIMEINTEVALL)
+    cursor.execute("DELETE FROM videos WHERE created_at < ?", (cutoff_time.strftime("%Y-%m-%d %H:%M:%S"), ))
 
+    #Delete Files in directory
+    delete_old_files("/service/data/videos/", DELETE_TIMEINTEVALL)
+    delete_old_files("/service/data/thumbnails/", DELETE_TIMEINTEVALL)
+    delete_old_files("/service/data/private/", DELETE_TIMEINTEVALL)
     connection.commit()
     connection.close()
 
--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -4,10 +4,11 @@ set -x
 
 sh db/start_db.sh
 # Chown the mounted data volume
+mkdir -p /service/data/videos /service/data/thumbnails /service/data/private
 chown -R service:service "../data/"
 chown -R service:service /service/data
 
-
+python3 /service/cleanup.py &
 
 # Launch our service as user 'service'
 exec su -s /bin/sh -c './target/release/backend' service
